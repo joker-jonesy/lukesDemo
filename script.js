958107af0decfcbe537b7b375d0347f494580e8f
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2020-09-15 14:30:32 -0400

    My first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var boxEle = document.body.querySelector(".box");
+
+var userInput = prompt("What is your name?");
+
+var numberOne = 8;
+var numberTwo = 2;
+
+boxEle.innerHTML=numberOne+numberTwo;
+
+boxEle.innerHTML=userInput;
